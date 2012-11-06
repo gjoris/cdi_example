ff720fbbe68968946359c3d3d7fc9598e601d0c8
Geroen Joris <geroen@gmail.com>
2012-11-06 12:28:59 +0100

    Commit SampleController

---
+++ cdi_example/src/main/java/eu/droidit/cdi_example/SampleController.java
@@ -0,0 +1,12 @@
+package eu.droidit.cdi_example;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: geroen
+ * Date: 6/11/12
+ * Time: 12:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class SampleController {
+
+}
